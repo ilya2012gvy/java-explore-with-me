06481d17e2ce231319e786ce967467fd326aa30a
ilya <ilea.morozov@yandex.ru>
2023-08-28 11:35:19 +0300

    Исправление ошибок-9.

--- stats/client/src/main/java/ru/practicum/StatsClient.java
+++ stats/client/src/main/java/ru/practicum/StatsClient.java
@@ -1,5 +1,7 @@
 package ru.practicum;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,14 +10,12 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class StatsClient extends BaseClient {
-    public static final String DT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
     public StatsClient(@Value("${ewm-stats-service}") String serverUrl, RestTemplateBuilder builder) {
@@ -31,22 +31,15 @@ public class StatsClient extends BaseClient {
         return post("/hit", hitDto);
     }
 
-    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        StringBuilder uriBuilder = new StringBuilder("/stats" + "?start={start}&end={end}");
+    public List<ViewStatsDto> getStatistic(String start, String end, List<String> uris, Boolean unique) {
         Map<String, Object> parameters = Map.of(
-                "start", start.format(DateTimeFormatter.ofPattern(DT_FORMAT)),
-                "end", end.format(DateTimeFormatter.ofPattern(DT_FORMAT))
+                "start", start,
+                "end", end,
+                "uris", String.join(",", uris),
+                "unique", unique
         );
-
-        if (uris != null && !uris.isEmpty()) {
-            for (String uri : uris) {
-                uriBuilder.append("&uris=").append(uri);
-            }
-        }
-        if (unique != null) {
-            uriBuilder.append("&unique=").append(unique);
-        }
-
-        return get(uriBuilder.toString(), parameters.size());
+        ResponseEntity<Object> response = get("/stats?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
+        return objectMapper.convertValue(response.getBody(), new TypeReference<>() {
+        });
     }
 }
